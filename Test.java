469ec13d9ca4b31e42c5663673b335c63e07f404
bbgethub <78780969+bbgethub@users.noreply.github.com>
2022-12-19 17:59:39 -0600

    Practice 2

---
+++ Test.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+
+public class Test {
+    public static void main(String arg[])
+    {
+        System.out.println("Hello");
+
+    }
+}
